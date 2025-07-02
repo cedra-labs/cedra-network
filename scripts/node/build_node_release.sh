47b8a1bbf5ecfd9cb8482e7ee26f1fa56554b3f4
m.reznichenko <>
2025-07-02 15:05:04 +0300

    added updated cedra node script for unix systems

--- scripts/node/build_node_release.sh
+++ scripts/node/build_node_release.sh
@@ -20,34 +20,43 @@ NAME='cedra-node'
 CARGO_PATH="$NAME/Cargo.toml"
 PLATFORM_NAME="$1"
 EXPECTED_VERSION="$2"
-SKIP_CHECKS="$3"
-COMPATIBILITY_MODE="$4"
+COMPATIBILITY_MODE="$3"
 
 # Grab system information
 ARCH=$(uname -m)
 OS=$(uname -s)
 VERSION=$(sed -n '/^\w*version = /p' "$CARGO_PATH" | sed 's/^.*=[ ]*"//g' | sed 's/".*$//g')
 
-if [[ "$SKIP_CHECKS" != "true" ]]; then
-  # Check that the version is well-formed, note that it should already be correct, but this double checks it
-  if ! [[ "$EXPECTED_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+# Check that the version is well-formed, note that it should already be correct, but this double checks it
+if ! [[ "$EXPECTED_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "$EXPECTED_VERSION is malformed, must be of the form '^[0-9]+\.[0-9]+\.[0-9]+$'"
     exit 1
-  fi
+fi
 
-  # Check that the version matches the Cargo.toml
-  if [[ "$EXPECTED_VERSION" != "$VERSION" ]]; then
+# Check that the version matches the Cargo.toml
+if [[ "$EXPECTED_VERSION" != "$VERSION" ]]; then
+    echo "Wanted to release for $EXPECTED_VERSION, but Cargo.toml says the version is $VERSION"
+fi
+
+echo "$VERSION vesion"
+
+if [[ "$VERSION" == "0.0.0" ] || [ "$VERSION" == "0.0.0-main" ]]; then
+    $VERSION=$EXPECTED_VERSION
+fi
+
+echo "$VERSION vesion"
+
+if [[ "$EXPECTED_VERSION" != "$VERSION" ]]; then
     echo "Wanted to release for $EXPECTED_VERSION, but Cargo.toml says the version is $VERSION"
     exit 2
-  fi
+fi
 
-  # Check that the release doesn't already exist
-  if curl -s --stderr /dev/null --output /dev/null --head -f "https://github.com/cedra-labs/cedra-network/releases/download/cedra-node-v$EXPECTED_VERSION/cedra-node-$EXPECTED_VERSION-Ubuntu-22.04-x86_64.zip"; then
+exit 0
+
+# Check that the release doesn't already exist
+if curl -s --stderr /dev/null --output /dev/null --head -f "https://github.com/cedra-labs/cedra-network/releases/download/cedra-node-v$EXPECTED_VERSION/cedra-node-$EXPECTED_VERSION-Ubuntu-22.04-x86_64.zip"; then
     echo "$EXPECTED_VERSION already released"
     exit 3
-  fi
-else
-  echo "WARNING: Skipping version checks!"
 fi
 
 echo "Building release $VERSION of $NAME for $OS-$PLATFORM_NAME on $ARCH"
