c19b93def4589b50a4b6876b16f426f6079855fe
Egor <82723465+Inkvisto@users.noreply.github.com>
2026-01-28 12:06:05 +0100

    Revert "READY: tmestamp & oracle too many open files fixes"

--- oracle/src/lib.rs
+++ oracle/src/lib.rs
@@ -26,25 +26,25 @@ pub fn start_oracles_runtime(
 ) -> Runtime {
     let runtime = cedra_runtimes::spawn_named_runtime("oracles".into(), Some(4));
     if auth_key.is_some() && oracles_updated_events.is_some() {
-        let whitelist = Arc::new(Whitelist::new(db_reader, indexer_reader));
+    let whitelist = Arc::new(Whitelist::new(db_reader, indexer_reader));
 
-        let mut oracle_price_manager = OraclePriceManager::new(
-            auth_key.unwrap(),
-            Arc::clone(&whitelist),
-            vtxn_pool,
-            oracles_updated_events.unwrap(),
-            chain_id,
-        );
+    let mut oracle_price_manager = OraclePriceManager::new(
+        auth_key.unwrap(),
+        Arc::clone(&whitelist),
+        vtxn_pool,
+        oracles_updated_events.unwrap(),
+        chain_id,
+    );
 
-        let (price_tx, price_rx) = mpsc::channel(1000);
+    let (price_tx, price_rx) = mpsc::channel(1000);
 
-        runtime.spawn(async move {
-            if let Err(e) = oracle_price_manager.run(price_rx, price_tx).await {
-                eprintln!("Oracle price manager start failed: {:?}", e);
-            }
-        });
+    runtime.spawn(async move {
+        if let Err(e) = oracle_price_manager.run(price_rx, price_tx).await {
+            eprintln!("Oracle price manager start failed: {:?}", e);
+        }
+    });
     } else {
-        tracing::warn!("Oracle runtime not started: auth key missing or events not exist on move");
+         tracing::warn!("Oracle runtime not started: auth key missing or events not exist on move");
     }
 
     runtime
